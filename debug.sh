f50924aa0f0d23398c9a79e83317b1576fdcc701
Chris Gregory <czipperz@gmail.com>
2020-03-01 22:37:34 -0800

    In debug.sh pass through arguments and use 30% space

--- debug.sh
+++ debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tmux splitw -v -p 50 "gdbserver localhost:12345 ./build/debug/mag"
+tmux splitw -v -p 30 "gdbserver localhost:12345 ./build/debug/mag $*"
 tmux selectp -t 0
 gdb -x debug.gdb
