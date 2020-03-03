2a9a200c79a26c0030ce8f8fa2c66596bb458620
Chris Gregory <czipperz@gmail.com>
2020-03-02 18:31:10 -0800

    command_open_file auto fills with current buffer location

--- src/commands.cpp
+++ src/commands.cpp
@@ -775,6 +775,28 @@ void command_open_file(Editor* editor, Command_Source source) {
     message.text = "Open file: ";
     message.response_callback = command_open_file_callback;
     message.response_callback_data = nullptr;
+
+    cz::String default_value = {};
+    CZ_DEFER(default_value.drop(cz::heap_allocator()));
+    bool has_default_value;
+    WITH_SELECTED_BUFFER({
+        has_default_value = buffer->path.find('/') != nullptr;
+        if (has_default_value) {
+            default_value = buffer->path.clone(cz::heap_allocator());
+        }
+    });
+
+    if (has_default_value) {
+        WITH_BUFFER(buffer, source.client->mini_buffer_id(), WITH_TRANSACTION({
+                        transaction.init(1, default_value.len());
+                        Edit edit;
+                        edit.value.init_duplicate(transaction.value_allocator(), default_value);
+                        edit.position = 0;
+                        edit.is_insert = true;
+                        transaction.push(edit);
+                    }));
+    }
+
     source.client->show_message(message);
 }
 
