17e31a2c250294acc22e4738d2e671b24131dfec
Chris Gregory <czipperz@gmail.com>
2020-03-03 21:51:44 -0800

    Handle PanicReachedException instead of crashing

--- mains/main.cpp
+++ mains/main.cpp
@@ -10,18 +10,24 @@
 using namespace mag;
 
 int main(int argc, char** argv) {
-    Server server = {};
-    CZ_DEFER(server.drop());
-    server.editor.create_buffer("*scratch*");
-    server.editor.key_map = create_key_map();
-    server.editor.theme = create_theme();
+    try {
+        Server server = {};
+        CZ_DEFER(server.drop());
+        server.editor.create_buffer("*scratch*");
+        server.editor.key_map = create_key_map();
+        server.editor.theme = create_theme();
 
-    Client client = server.make_client();
-    CZ_DEFER(client.drop());
+        Client client = server.make_client();
+        CZ_DEFER(client.drop());
 
-    if (argc == 2) {
-        open_file(&server.editor, &client, argv[1]);
+        if (argc == 2) {
+            open_file(&server.editor, &client, argv[1]);
+        }
+
+        run_ncurses(&server, &client);
+        return 0;
+    } catch (cz::PanicReachedException& ex) {
+        fprintf(stderr, "Fatal error: %s\n", ex.what());
+        return 1;
     }
-
-    run_ncurses(&server, &client);
 }
