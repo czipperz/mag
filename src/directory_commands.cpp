225ad2535afdb651c1e6c08c70cc13093994f39b
Chris Gregory <czipperz@gmail.com>
2020-03-12 19:38:58 -0400

    Implement command_directory_open_path

--- src/directory_commands.cpp
+++ src/directory_commands.cpp
@@ -1,18 +1,28 @@
 #include "directory_commands.hpp"
 
-#include "command.hpp"
-#include "editor.hpp"
-
 #include "client.hpp"
-#include "message.hpp"
+#include "command.hpp"
+#include "command_macros.hpp"
+#include "contents.hpp"
+#include "editor.hpp"
+#include "file.hpp"
+#include "movement.hpp"
 
 namespace mag {
 
 void command_directory_open_path(Editor* editor, Command_Source source) {
-    Message message = {};
-    message.tag = Message::SHOW;
-    message.text = "File path must not be empty";
-    source.client->show_message(message);
+    WITH_SELECTED_BUFFER({
+        Contents_Iterator start = buffer->contents.iterator_at(buffer->cursors[0].point);
+        Contents_Iterator end = start;
+        start_of_line(buffer, &start);
+        end_of_line(buffer, &end);
+
+        if (start.position < end.position) {
+            SSOStr str = buffer->contents.slice(cz::heap_allocator(), start.position, end.position);
+            CZ_DEFER(str.drop(cz::heap_allocator()));
+            open_file(editor, source.client, str.as_str());
+        }
+    });
 }
 
 }
