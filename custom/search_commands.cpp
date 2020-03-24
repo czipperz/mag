0df1159675ff41cd6d10d4e2dbaae6e970563e42
Chris Gregory <czipperz@gmail.com>
2020-03-23 23:49:52 -0400

    Implement command_search_open

--- custom/search_commands.cpp
+++ custom/search_commands.cpp
@@ -1,62 +1,80 @@
 #include "search_commands.hpp"
 
+#include <cz/defer.hpp>
+#include <cz/heap.hpp>
 #include "command_macros.hpp"
+#include "file.hpp"
 #include "movement.hpp"
 
 namespace mag {
 namespace custom {
 
+static bool eat_until_colon(Contents_Iterator* iterator) {
+    while (1) {
+        if (iterator->at_eob()) {
+            return false;
+        }
+        if (iterator->get() == ':') {
+            return true;
+        }
+        iterator->advance();
+    }
+}
+
+static bool parse_number(Contents_Iterator* iterator, uint64_t* num) {
+    iterator->advance();
+    while (1) {
+        if (iterator->at_eob()) {
+            return false;
+        }
+        char ch = iterator->get();
+        if (ch == ':') {
+            return true;
+        }
+        if (!isdigit(ch)) {
+            return false;
+        }
+        *num *= 10;
+        *num += ch - '0';
+        iterator->advance();
+    }
+}
+
 void command_search_open(Editor* editor, Command_Source source) {
     WITH_SELECTED_BUFFER({
-        Contents_Iterator base_end = buffer->contents.iterator_at(0);
-        while (1) {
-            if (base_end.at_eob()) {
-                return;
-            }
-            if (base_end.get() == ':') {
-                break;
-            }
-            base_end.advance();
-        }
-
         Contents_Iterator relative_start = buffer->contents.iterator_at(window->cursors[0].point);
         start_of_line(&relative_start);
         Contents_Iterator relative_end = relative_start;
-        while (1) {
-            if (relative_end.at_eob()) {
-                return;
-            }
-            if (relative_end.get() == ':') {
-                break;
-            }
-            relative_end.advance();
+        if (!eat_until_colon(&relative_end)) {
+            return;
         }
 
-        Contents_Iterator line_start = relative_end;
-        line_start.advance();
-        Contents_Iterator line_end = line_start;
-        while (1) {
-            if (line_end.at_eob()) {
-                return;
-            }
-            if (line_end.get() == ':') {
-                break;
-            }
-            line_end.advance();
+        Contents_Iterator iterator = relative_end;
+        uint64_t line = 0;
+        uint64_t column = 0;
+        if (!parse_number(&iterator, &line)) {
+            return;
+        }
+        if (!parse_number(&iterator, &column)) {
+            return;
         }
 
-        Contents_Iterator column_start = line_end;
-        column_start.advance();
-        Contents_Iterator column_end = column_start;
-        while (1) {
-            if (column_end.at_eob()) {
-                return;
-            }
-            if (column_end.get() == ':') {
-                break;
-            }
-            column_end.advance();
+        Contents_Iterator base_end = buffer->contents.start();
+        if (!eat_until_colon(&base_end)) {
+            return;
         }
+
+        cz::String path = {};
+        path.reserve(cz::heap_allocator(),
+                     base_end.position + 1 + relative_end.position - relative_start.position);
+        CZ_DEFER(path.drop(cz::heap_allocator()));
+        buffer->contents.slice_into(buffer->contents.start(), base_end.position, path.end());
+        path.set_len(path.len() + base_end.position);
+        path.push('/');
+        buffer->contents.slice_into(relative_start, relative_end.position, path.end());
+        path.set_len(path.len() + relative_end.position - relative_start.position);
+
+        open_file(editor, source.client, path);
     });
 }
 
