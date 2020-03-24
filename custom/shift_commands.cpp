a745f3133517b40f60806f38aa79939402d8c243
Chris Gregory <czipperz@gmail.com>
2020-03-24 02:17:46 -0400

    Simplify shift line functions

--- custom/shift_commands.cpp
+++ custom/shift_commands.cpp
@@ -10,223 +10,129 @@ void command_shift_line_forward(Editor* editor, Command_Source source) {
     WITH_SELECTED_BUFFER();
     cz::Slice<Cursor> cursors = window->cursors;
 
-    cz::Vector<uint64_t> cursor_positions = {};
-    CZ_DEFER(cursor_positions.drop(cz::heap_allocator()));
-    cursor_positions.reserve(cz::heap_allocator(), cursors.len);
-
     uint64_t sum_line_lengths = 0;
+    size_t num_edits = cursors.len * 2;
     for (size_t c = 0; c < cursors.len; ++c) {
-        Contents_Iterator start = buffer->contents.iterator_at(cursors[c].point);
-        Contents_Iterator end = start;
-        start_of_line(&start);
-        end_of_line(&end);
-        sum_line_lengths += end.position - start.position + 1;
+        Contents_Iterator start_next = buffer->contents.iterator_at(cursors[c].point);
+        end_of_line(&start_next);
+        forward_char(&start_next);
+        if (start_next.at_eob()) {
+            num_edits -= 2;
+            continue;
+        }
+
+        Contents_Iterator end_next = start_next;
+        end_of_line(&end_next);
+        sum_line_lengths += end_next.position - start_next.position;
     }
 
     Transaction transaction;
-    transaction.init(cursors.len * 3, (size_t)sum_line_lengths);
+    transaction.init(num_edits, (size_t)sum_line_lengths + num_edits);
     CZ_DEFER(transaction.drop());
 
     for (size_t c = 0; c < cursors.len; ++c) {
         Contents_Iterator start = buffer->contents.iterator_at(cursors[c].point);
-        Contents_Iterator end = start;
+        Contents_Iterator start_next = start;
+        end_of_line(&start_next);
+        forward_char(&start_next);
+        if (start_next.at_eob()) {
+            continue;
+        }
+
         start_of_line(&start);
-        end_of_line(&end);
-        uint64_t column = cursors[c].point - start.position;
+        Contents_Iterator end_next = start_next;
+        end_of_line(&end_next);
 
-        Contents_Iterator insertion_point = end;
-        forward_char(&insertion_point);
-        end_of_line(&insertion_point);
-        if (insertion_point.position == end.position) {
-            continue;
-        }
+        //     def\nghi
+        //     [    [  )
+        // start    ^  ^
+        // start_next  end_next
+        char* buf = (char*)transaction.value_allocator()
+                        .alloc({end_next.position - start_next.position + 2, 1})
+                        .buffer;
+        buf[0] = '\n';
+        buf[end_next.position - start_next.position + 1] = '\n';
+        buffer->contents.slice_into(start_next, end_next.position, buf + 1);
 
-        // abc\ndef\nghi\njkl\n
-        //      [  )    ^
-        //  start  end  ^
-        //       insertion_point
+        Edit remove;
+        remove.value.init_from_constant({buf, end_next.position - start_next.position + 1});
+        remove.position = start_next.position - 1;
+        remove.is_insert = false;
+        transaction.push(remove);
 
-        cursor_positions.push(insertion_point.position - (end.position - start.position) + column);
-
-        if (!insertion_point.at_eob()) {
-            // abc\ndef\nghi\njkl\n
-            //      [    )    ^
-            //  start    end  ^
-            //         insertion_point
-            insertion_point.advance();
-            end.advance();
-        } else if (!start.at_bob()) {
-            // abc\ndef\nghi
-            //    [    )    ^
-            //  start end   ^
-            //       insertion_point
-            start.retreat();
-        } else {
-            //    abc\ndef
-            //    [    )  ^
-            // start  end ^
-            //     insertion_point
-            end.advance();
-
-            SSOStr value =
-                buffer->contents.slice(transaction.value_allocator(), start, end.position);
-
-            Edit edit_insert;
-            cz::Str value_str = value.as_str();
-            edit_insert.value.init_from_constant({value_str.buffer, value_str.len - 1});
-            edit_insert.position = insertion_point.position;
-            edit_insert.is_insert = true;
-            transaction.push(edit_insert);
-
-            Edit edit_insert_newline;
-            edit_insert_newline.value.init_char('\n');
-            edit_insert_newline.position = insertion_point.position;
-            edit_insert_newline.is_insert = true;
-            transaction.push(edit_insert_newline);
-
-            Edit edit_delete;
-            edit_delete.value = value;
-            edit_delete.position = start.position;
-            edit_delete.is_insert = false;
-            transaction.push(edit_delete);
-            continue;
-        }
-
-        SSOStr value = buffer->contents.slice(transaction.value_allocator(), start, end.position);
-
-        Edit edit_insert;
-        edit_insert.value = value;
-        edit_insert.position = insertion_point.position;
-        edit_insert.is_insert = true;
-        transaction.push(edit_insert);
-
-        Edit edit_delete;
-        edit_delete.value = value;
-        edit_delete.position = start.position;
-        edit_delete.is_insert = false;
-        transaction.push(edit_delete);
+        Edit insert;
+        insert.value.init_from_constant({buf + 1, end_next.position - start_next.position + 1});
+        insert.position = start.position;
+        insert.is_insert = true;
+        transaction.push(insert);
     }
 
     transaction.commit(buffer);
-
-    window->update_cursors(buffer->changes);
-
-    for (size_t c = 0; c < cursors.len; ++c) {
-        cursors[c].point = cursor_positions[c];
-    }
 }
 
 void command_shift_line_backward(Editor* editor, Command_Source source) {
     WITH_SELECTED_BUFFER();
     cz::Slice<Cursor> cursors = window->cursors;
 
-    cz::Vector<uint64_t> cursor_positions = {};
-    CZ_DEFER(cursor_positions.drop(cz::heap_allocator()));
-    cursor_positions.reserve(cz::heap_allocator(), cursors.len);
-
     uint64_t sum_line_lengths = 0;
+    size_t num_edits = cursors.len * 2;
     for (size_t c = 0; c < cursors.len; ++c) {
-        Contents_Iterator start = buffer->contents.iterator_at(cursors[c].point);
-        Contents_Iterator end = start;
-        start_of_line(&start);
-        end_of_line(&end);
-        sum_line_lengths += end.position - start.position + 1;
+        Contents_Iterator end_prev = buffer->contents.iterator_at(cursors[c].point);
+        start_of_line(&end_prev);
+        backward_char(&end_prev);
+        if (end_prev.at_bob()) {
+            num_edits -= 2;
+            continue;
+        }
+
+        Contents_Iterator start_prev = end_prev;
+        start_of_line(&start_prev);
+        sum_line_lengths += end_prev.position - start_prev.position;
     }
 
     Transaction transaction;
-    transaction.init(cursors.len * 3, (size_t)sum_line_lengths);
+    transaction.init(num_edits, (size_t)sum_line_lengths + num_edits);
     CZ_DEFER(transaction.drop());
 
     for (size_t c = 0; c < cursors.len; ++c) {
-        Contents_Iterator start = buffer->contents.iterator_at(cursors[c].point);
-        Contents_Iterator end = start;
-        start_of_line(&start);
+        Contents_Iterator end = buffer->contents.iterator_at(cursors[c].point);
+        Contents_Iterator end_prev = end;
+        start_of_line(&end_prev);
+        backward_char(&end_prev);
+        if (end_prev.at_bob()) {
+            num_edits -= 2;
+            continue;
+        }
+
         end_of_line(&end);
-        uint64_t column = cursors[c].point - start.position;
+        Contents_Iterator start_prev = end_prev;
+        start_of_line(&start_prev);
 
-        Contents_Iterator insertion_point = start;
-        backward_line(&insertion_point);
-        if (insertion_point.position == start.position) {
-            continue;
-        }
+        //          def\nghi
+        //          [  )    )
+        //          ^  ^    end
+        // start_prev  end_prev
+        char* buf = (char*)transaction.value_allocator()
+                        .alloc({end_prev.position - start_prev.position + 2, 1})
+                        .buffer;
+        buf[0] = '\n';
+        buf[end_prev.position - start_prev.position + 1] = '\n';
+        buffer->contents.slice_into(start_prev, end_prev.position, buf + 1);
 
-        // abc\ndef\nghi\njkl\n
-        //      ^    [  )
-        //      ^ start end
-        // insertion_point
+        Edit insert;
+        insert.value.init_from_constant({buf, end_prev.position - start_prev.position + 1});
+        insert.position = end.position;
+        insert.is_insert = true;
+        transaction.push(insert);
 
-        cursor_positions.push(insertion_point.position + column);
-
-        if (!end.at_eob()) {
-            // Normal case: pick up newline after end.
-            //
-            //   abc\ndef\nghi\njkl\n
-            //        ^    [    )
-            //        ^  start end
-            // insertion_point
-            end.advance();
-        } else if (!insertion_point.at_bob()) {
-            // We don't have the line after us.
-            //
-            // abc\ndef\nghi
-            //    ^    [    )
-            //    ^ start   end
-            // insertion_point
-            start.retreat();
-            insertion_point.retreat();
-        } else {
-            // We don't have a line after us or before us.
-            //
-            // abc\ndef
-            //      [  )
-            start.retreat();
-
-            SSOStr value =
-                buffer->contents.slice(transaction.value_allocator(), start, end.position);
-
-            Edit edit_delete;
-            edit_delete.value = value;
-            edit_delete.position = start.position;
-            edit_delete.is_insert = false;
-            transaction.push(edit_delete);
-
-            Edit edit_insert;
-            cz::Str value_str = value.as_str();
-            edit_insert.value.init_from_constant({value_str.buffer + 1, value_str.len - 1});
-            edit_insert.position = insertion_point.position;
-            edit_insert.is_insert = true;
-            transaction.push(edit_insert);
-
-            Edit edit_insert_newline;
-            edit_insert_newline.value.init_char('\n');
-            edit_insert_newline.position = value_str.len - 1;
-            edit_insert_newline.is_insert = true;
-            transaction.push(edit_insert_newline);
-            continue;
-        }
-
-        SSOStr value = buffer->contents.slice(transaction.value_allocator(), start, end.position);
-
-        Edit edit_delete;
-        edit_delete.value = value;
-        edit_delete.position = start.position;
-        edit_delete.is_insert = false;
-        transaction.push(edit_delete);
-
-        Edit edit_insert;
-        edit_insert.value = value;
-        edit_insert.position = insertion_point.position;
-        edit_insert.is_insert = true;
-        transaction.push(edit_insert);
+        Edit remove;
+        remove.value.init_from_constant({buf + 1, end_prev.position - start_prev.position + 1});
+        remove.position = start_prev.position;
+        remove.is_insert = false;
+        transaction.push(remove);
     }
 
     transaction.commit(buffer);
-
-    window->update_cursors(buffer->changes);
-
-    for (size_t c = 0; c < cursors.len; ++c) {
-        cursors[c].point = cursor_positions[c];
-    }
 }
 
 }
