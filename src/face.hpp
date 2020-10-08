34b03358e0b737e627a48009575ae1b828394665
Chris Gregory <czipperz@gmail.com>
2020-10-08 14:22:47 -0500

    Extract Face, Face_Color, and Color to face.hpp

---
+++ src/face.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace mag {
+
+struct Color {
+    uint8_t r, g, b;
+
+    constexpr bool operator==(const Color& other) const {
+        return r == other.r && g == other.g && b == other.b;
+    }
+    constexpr bool operator!=(const Color& other) const { return !(*this == other); }
+};
+
+struct Face_Color {
+    bool is_themed;
+    union X {
+        int16_t theme_index;
+        Color color;
+
+        constexpr X(int16_t theme_index) : theme_index(theme_index) {}
+        constexpr X(Color color) : color(color) {}
+    } x;
+
+    constexpr Face_Color() : Face_Color(-1) {}
+
+    constexpr Face_Color(int16_t theme_index) : is_themed(true), x(theme_index) {}
+
+    constexpr Face_Color(Color color) : is_themed(false), x(color) {}
+
+    constexpr bool operator==(const Face_Color& other) const {
+        if (is_themed) {
+            return other.is_themed && x.theme_index == other.x.theme_index;
+        } else {
+            return !other.is_themed && x.color == other.x.color;
+        }
+    }
+    constexpr bool operator!=(const Face_Color& other) const { return !(*this == other); }
+};
+
+struct Face {
+    Face_Color foreground;
+    Face_Color background;
+
+    enum Flags {
+        BOLD = 1,
+        UNDERSCORE = 2,
+        REVERSE = 4,
+        ITALICS = 8,
+        INVISIBLE = 16,
+    };
+    uint32_t flags = 0;
+};
+
+}
--- src/theme.hpp
+++ src/theme.hpp
@@ -3,59 +3,11 @@
 #include <stdint.h>
 #include <cz/vector.hpp>
 #include "completion.hpp"
+#include "face.hpp"
 
 namespace mag {
 struct Decoration;
 struct Overlay;
-
-struct Color {
-    uint8_t r, g, b;
-
-    constexpr bool operator==(const Color& other) const {
-        return r == other.r && g == other.g && b == other.b;
-    }
-    constexpr bool operator!=(const Color& other) const { return !(*this == other); }
-};
-
-struct Face_Color {
-    bool is_themed;
-    union X {
-        int16_t theme_index;
-        Color color;
-
-        constexpr X(int16_t theme_index) : theme_index(theme_index) {}
-        constexpr X(Color color) : color(color) {}
-    } x;
-
-    constexpr Face_Color() : Face_Color(-1) {}
-
-    constexpr Face_Color(int16_t theme_index) : is_themed(true), x(theme_index) {}
-
-    constexpr Face_Color(Color color) : is_themed(false), x(color) {}
-
-    constexpr bool operator==(const Face_Color& other) const {
-        if (is_themed) {
-            return other.is_themed && x.theme_index == other.x.theme_index;
-        } else {
-            return !other.is_themed && x.color == other.x.color;
-        }
-    }
-    constexpr bool operator!=(const Face_Color& other) const { return !(*this == other); }
-};
-
-struct Face {
-    Face_Color foreground;
-    Face_Color background;
-
-    enum Flags {
-        BOLD = 1,
-        UNDERSCORE = 2,
-        REVERSE = 4,
-        ITALICS = 8,
-        INVISIBLE = 16,
-    };
-    uint32_t flags = 0;
-};
 
 struct Theme {
     /// The colors to be defined.  Note that they are not always available.
