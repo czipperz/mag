d1fe5e9d9fd8659e46b4c6152121bee56d4c1976
Chris Gregory <czipperz@gmail.com>
2020-12-17 03:57:21 -0600

    Use set -e instead of return in build scripts

--- build-release-debug.sh
+++ build-release-debug.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+set -e
+
 "$(dirname "$0")"/build-wrapper.sh build/release-debug RelWithDebInfo
-return
--- build-tracy.sh
+++ build-tracy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+set -e
+
 "$(dirname "$0")"/build-wrapper.sh build/tracy RelWithDebInfo -DTRACY_ENABLE=1
-return
