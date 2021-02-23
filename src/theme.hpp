9ccbc87fab38dade7d92bd5f79e7106c8d67687f
Czipperz <czipperz@gmail.com>
2021-02-22 21:58:59 -0800

    Add indentation configuration to Theme

--- src/theme.hpp
+++ src/theme.hpp
@@ -42,6 +42,35 @@ struct Theme {
     Completion_Filter mini_buffer_completion_filter;
     Completion_Filter window_completion_filter;
 
+    /// The number of columns in a level of indentation.
+    ///
+    /// To indent 2 spaces but then 8 spaces are converted to tabs use:
+    /// ```
+    /// indent_columns = 2;
+    /// tab_column_width = 8;
+    /// ever_use_tabs = true;
+    /// ```
+    ///
+    /// To indent 4 spaces and never use tabs use:
+    /// ```
+    /// indent_columns = 4;
+    /// // tab_column_width is arbitrary
+    /// ever_use_tabs = false;
+    /// ```
+    ///
+    /// To indent using one tab and display tabs as 4 wide use:
+    /// ```
+    /// indent_columns = tab_column_width = 4;
+    /// ever_use_tabs = true;
+    /// ```
+    uint32_t indent_columns = 4;
+
+    /// The number of columns a tab takes up.
+    uint32_t tab_column_width = 4;
+
+    /// If `true`, prefers tabs over spaces as long as they fit.
+    bool ever_use_tabs = false;
+
     void drop(cz::Allocator allocator) {
         faces.drop(allocator);
     }
