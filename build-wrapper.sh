1bfb846a155f2a464ff7ba5ac9ec54587afd949e
Czipperz <czipperz@gmail.com>
2021-03-07 12:00:07 -0800

    Extract handling vcvars64.bat out of run-build.bat to build-wrapper.sh

--- build-wrapper.sh
+++ build-wrapper.sh
@@ -13,7 +13,22 @@ mkdir -p "$directory"
 cd "$directory"
 
 if [ "$OSTYPE" == "msys" ]; then
-    "$root"/run-build.bat "$@"
+    VC_INSTALL_DIR="${VC_INSTALL_DIR:-'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community'}"
+    VARS="$VC_INSTALL_DIR"'\VC\Auxiliary\Build\vcvars64.bat'
+
+    if [ ! -e "$VARS" ]; then
+        echo "Could not find vcvars64.bat at:"
+        echo "    $VARS"
+        echo
+        echo "If you have Visual Studio installed, please specify VC_INSTALL_DIR to"
+        echo "reflect the correct path to vcvars64.bat.  It is assumed to be:"
+        echo "    $VC_INSTALL_DIR"
+        echo
+        echo "Otherwise, please install Visual Studio."
+        exit 1
+    fi
+
+    VARS="$VARS" "$root"/run-build.bat "$@"
 else
     "$root"/run-build.sh "$@"
 fi
