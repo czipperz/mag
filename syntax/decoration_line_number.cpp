d21eedcc71cce7f08481a3a52a266e8d16f69850
Chris Gregory <czipperz@gmail.com>
2021-03-14 18:25:04 -0500

    Optimize decoration line number

--- syntax/decoration_line_number.cpp
+++ syntax/decoration_line_number.cpp
@@ -7,10 +7,41 @@
 #include <cz/write.hpp>
 #include "buffer.hpp"
 #include "decoration.hpp"
+#include "movement.hpp"
 #include "window.hpp"
 
 namespace mag {
 namespace syntax {
+
+struct Buffer_Data {
+    Buffer_Id id;
+    size_t change_index;
+    uint64_t position;
+    uint64_t line_number;
+};
+
+struct Data {
+    cz::Vector<Buffer_Data> buffer_datas;
+};
+
+static void update_insert(const Edit& edit, Buffer_Data* data) {
+    if (data->position > edit.position ||
+        (!(edit.flags & Edit::AFTER_POSITION_MASK) && data->position == edit.position)) {
+        data->position += edit.value.len();
+        data->line_number += edit.value.as_str().count('\n');
+    }
+}
+
+static void update_remove(const Edit& edit, Buffer_Data* data) {
+    if (edit.position + edit.value.len() <= data->position) {
+        data->position -= edit.value.len();
+        data->line_number -= edit.value.as_str().count('\n');
+    } else if (edit.position < data->position) {
+        cz::Str str = edit.value.as_str().slice_end(data->position - edit.position);
+        data->position -= str.len;
+        data->line_number -= str.count('\n');
+    }
+}
 
 static bool decoration_line_number_append(Buffer* buffer,
                                           Window_Unified* window,
@@ -18,27 +49,98 @@ static bool decoration_line_number_append(Buffer* buffer,
                                           void* _data) {
     ZoneScoped;
 
-    uint64_t line_number = 0;
-    uint64_t line_start = 0;
-    Contents_Iterator iterator = buffer->contents.start();
-    while (iterator.position < window->cursors[0].point) {
-        if (iterator.get() == '\n') {
-            ++line_number;
-            line_start = iterator.position + 1;
+    Data* data = (Data*)_data;
+    Buffer_Data* bd = nullptr;
+    for (size_t i = 0; i < data->buffer_datas.len(); ++i) {
+        if (data->buffer_datas[i].id == window->id) {
+            for (size_t c = data->buffer_datas[i].change_index; c < buffer->changes.len(); ++c) {
+                auto& change = buffer->changes[c];
+                if (change.is_redo) {
+                    for (size_t e = 0; e < change.commit.edits.len; ++e) {
+                        if (change.commit.edits[e].flags & Edit::INSERT_MASK) {
+                            update_insert(change.commit.edits[e], &data->buffer_datas[i]);
+                        } else {
+                            update_remove(change.commit.edits[e], &data->buffer_datas[i]);
+                        }
+                    }
+                } else {
+                    for (size_t e = change.commit.edits.len; e-- > 0;) {
+                        if (change.commit.edits[e].flags & Edit::INSERT_MASK) {
+                            update_remove(change.commit.edits[e], &data->buffer_datas[i]);
+                        } else {
+                            update_insert(change.commit.edits[e], &data->buffer_datas[i]);
+                        }
+                    }
+                }
+            }
+
+            data->buffer_datas[i].change_index = buffer->changes.len();
+
+            bd = &data->buffer_datas[i];
+            break;
+        }
+    }
+
+    if (!bd) {
+        Buffer_Data buffer_data;
+        buffer_data.id = window->id;
+        buffer_data.change_index = buffer->changes.len();
+        buffer_data.position = 0;
+        buffer_data.line_number = 0;
+
+        data->buffer_datas.reserve(cz::heap_allocator(), 1);
+        data->buffer_datas.push(buffer_data);
+
+        bd = &data->buffer_datas.last();
+    }
+
+    // It's faster to just iterate from the start of the file.
+    if (bd->position > window->cursors[0].point * 2) {
+        bd->position = 0;
+        bd->line_number = 0;
+    }
+
+    Contents_Iterator iterator = buffer->contents.iterator_at(bd->position);
+    if (iterator.position < window->cursors[0].point) {
+        while (iterator.position < window->cursors[0].point) {
+            if (iterator.get() == '\n') {
+                ++bd->line_number;
+            }
+            iterator.advance();
+        }
+    } else if (iterator.position > window->cursors[0].point) {
+        iterator.retreat();
+        while (iterator.position >= window->cursors[0].point) {
+            if (iterator.get() == '\n') {
+                --bd->line_number;
+            }
+            iterator.retreat();
         }
         iterator.advance();
     }
+    bd->position = iterator.position;
 
-    write(string_writer(string), 'L', line_number + 1, " C", iterator.position - line_start + 1);
+    write(string_writer(string), 'L', bd->line_number + 1);
+
+    Contents_Iterator line_start = iterator;
+    start_of_line(&line_start);
+    write(string_writer(string), " C", iterator.position - line_start.position + 1);
+
     return true;
 }
 
-static void decoration_line_number_cleanup(void* _data) {}
+static void decoration_line_number_cleanup(void* _data) {
+    Data* data = (Data*)_data;
+    data->buffer_datas.drop(cz::heap_allocator());
+    free(data);
+}
 
 Decoration decoration_line_number() {
+    Data* data = (Data*)calloc(1, sizeof(Data));
+    CZ_ASSERT(data);
     static const Decoration::VTable vtable = {decoration_line_number_append,
                                               decoration_line_number_cleanup};
-    return {&vtable, nullptr};
+    return {&vtable, data};
 }
 
 }
