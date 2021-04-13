56473a5adf41cf25fabc1599123ec9a3cd888a26
Chris Gregory <czipperz@gmail.com>
2021-04-12 19:37:01 -0700

    Show proportion of cursors that are visible

--- syntax/decoration_cursor_count.cpp
+++ syntax/decoration_cursor_count.cpp
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <Tracy.hpp>
 #include <algorithm>
+#include <cz/defer.hpp>
 #include <cz/heap.hpp>
 #include <cz/vector.hpp>
 #include <cz/write.hpp>
 #include "buffer.hpp"
 #include "decoration.hpp"
 #include "movement.hpp"
+#include "visible_region.hpp"
 #include "window.hpp"
 
 namespace mag {
@@ -24,7 +26,27 @@ static bool decoration_cursor_count_append(const Buffer* buffer,
         return false;
     }
 
-    write(string_writer(string), "(", window->cursors.len(), ")");
+    Contents_Iterator visible_start = buffer->contents.iterator_at(window->start_position);
+    Contents_Iterator visible_end = visible_start;
+    {
+        window->rows += 1;
+        CZ_DEFER(window->rows -= 1);
+        compute_visible_end(window, &visible_end);
+    }
+
+    size_t visible = 0;
+    for (size_t i = 0; i < window->cursors.len(); ++i) {
+        if (window->cursors[i].point >= visible_start.position &&
+            window->cursors[i].point <= visible_end.position) {
+            ++visible;
+        }
+    }
+
+    write(string_writer(string), "(", visible);
+    if (visible != window->cursors.len()) {
+        write(string_writer(string), "/", window->cursors.len());
+    }
+    write(string_writer(string), ")");
     return true;
 }
 
