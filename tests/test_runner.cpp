2dcb30e654166c6ccd9ba16acbea78fdad130cdf
Chris Gregory <czipperz@gmail.com>
2021-05-21 21:34:01 -0700

    Add test_runner module

---
+++ tests/test_runner.cpp
@@ -0,0 +1,97 @@
+#include "test_runner.hpp"
+#include "syntax/tokenize_general.hpp"
+
+namespace mag {
+
+Test_Runner::Test_Runner() {
+    buffer_array.init();
+
+    server = {};
+    server.init();
+
+    Buffer test_buffer = {};
+    test_buffer.type = Buffer::TEMPORARY;
+    test_buffer.name = cz::Str("*test*").duplicate(cz::heap_allocator());
+    server.editor.create_buffer(test_buffer);
+
+    client = server.make_client();
+
+    Window_Unified* window = client.selected_window();
+    Editor* editor = &server.editor;
+    WITH_WINDOW_BUFFER(window);
+    buffer->mode.next_token = syntax::general_next_token;
+}
+
+Test_Runner::~Test_Runner() {
+    client.drop();
+    server.drop();
+    buffer_array.drop();
+}
+
+void Test_Runner::setup(cz::Str input) {
+    Editor* editor = &server.editor;
+    WITH_SELECTED_BUFFER(&client);
+
+    Transaction transaction;
+    transaction.init(buffer);
+    CZ_DEFER(transaction.drop());
+
+    cz::String contents = {};
+
+    size_t offset = 0;
+    for (size_t i = 0; i < input.len; ++i) {
+        if (input[i] == '|') {
+            window->cursors.reserve(cz::heap_allocator(), 1);
+            Cursor cursor = {};
+            cursor.point = i - offset;
+            window->cursors.push(cursor);
+            ++offset;
+        } else {
+            contents.reserve(transaction.value_allocator(), 1);
+            contents.push(input[i]);
+        }
+    }
+
+    if (window->cursors.len() > 1) {
+        window->cursors.remove(0);
+    }
+
+    Edit edit;
+    edit.value = SSOStr::from_constant(contents);
+    edit.position = 0;
+    edit.flags = Edit::INSERT;
+    transaction.push(edit);
+
+    transaction.commit();
+
+    window->change_index = buffer->changes.len();
+}
+
+cz::String Test_Runner::stringify() {
+    cz::String output = {};
+
+    Editor* editor = &server.editor;
+    WITH_CONST_SELECTED_BUFFER(&client);
+
+    Contents_Iterator it = buffer->contents.start();
+    for (size_t i = 0; i < window->cursors.len(); ++i) {
+        uint64_t end = window->cursors[i].point;
+        while (it.position < end) {
+            output.reserve(allocator(), 1);
+            output.push(it.get());
+            it.advance();
+        }
+        output.reserve(allocator(), 1);
+        output.push('|');
+    }
+
+    return output;
+}
+
+void Test_Runner::run(Command_Function command) {
+    Command_Source source = {};
+    source.client = &client;
+    command(&server.editor, source);
+}
+
+}
---
+++ tests/test_runner.hpp
@@ -0,0 +1,34 @@
+#include <cz/buffer_array.hpp>
+#include <cz/defer.hpp>
+#include <cz/heap_string.hpp>
+#include <czt/test_base.hpp>
+#include "client.hpp"
+#include "command_macros.hpp"
+#include "server.hpp"
+
+namespace mag {
+
+struct Test_Runner {
+    Server server;
+    Client client;
+    cz::Buffer_Array buffer_array;
+
+    Test_Runner();
+    ~Test_Runner();
+    Test_Runner(const Test_Runner&) = delete;
+    Test_Runner& operator=(const Test_Runner&) = delete;
+
+    /// `input` should have `|` to represent cursors;
+    /// other characters will be inserted into the buffer.
+    void setup(cz::Str input);
+
+    /// Stringify the buffer's contents, adding `|` for each cursor.
+    cz::String stringify();
+
+    /// Run a command.
+    void run(Command_Function command);
+
+    cz::Allocator allocator() { return buffer_array.allocator(); }
+};
+
+}
