deefa5b2cf44e303bddecefd0b7210c005717aae
Chris Gregory <czipperz@gmail.com>
2021-05-31 17:25:19 -0500

    Rewrite splash page

--- mains/main.cpp
+++ mains/main.cpp
@@ -255,29 +255,37 @@ Mag: A customizable text editor.             gg\n\
      ggg.             .g.          .g.     ggg\n\
          ^ggggggggggg^   ^gggggggg^   ^gggg^\n\
 \n\
-Main key bindings:\n\
 \n\
-C-g     Stop action          C-o     Open file\n\
-F1      View key bindings    C-x     Run command by name\n\
+Mag is a WSIWYG editor -- typing text inserts text in the buffer, using Control or Alt modifiers\n\
+runs commands.  Control is written as C-* and Alt is written as A-*.  Mag is moderately similar\n\
+to Emacs but prefers Alt as a prefix over Control as it is easier to type on modern keyboards.\n\
+\n\
+Some basic key bindings are listed below.  Press F1 to view all key bindings.\n\
+Configuration should be done by editing `custom/config.cpp`.\n\
+\n\
+\n\
+Main key bindings:\n\
+C-o     Open file            C-g     Stop action (cancel prompt, delete cursors, stop selecting region)\n\
+\n\
 A-o     Go to other window   A-x b   Switch to open buffer\n\
 A-x 3   Split vertically     A-x 2   Split horizontally\n\
 A-/     Undo                 C-/     Redo\n\
 \n\
-A-n     Move down line       C-A-n   Create cursor down (line or search)\n\
-A-p     Move up line         C-A-p   Create cursor up (line or search)\n\
-A-q     Down matching token  C-q     Create cursor next matching token\n\
-A-j     Up matching token    C-j     Create cursor previous matching token\n\
-A-r     Search forward       C-r     Search reverse\n\
+C-SPACE Select region        A-w     Copy region\n\
+A-y     Paste                C-y     Paste previous (used after A-y or C-y in tandem)\n\
 \n\
-C-SPACE Select region        TAB     Increase indent level\n\
-A-w     Copy region          A-y     Paste (C-y to cycle paste)\n\
+C-f     Forward char         C-b     Backward char\n\
+A-f     Forward word         A-b     Backward word\n\
+C-A-f   Forward token        C-A-b   Backward token\n\
+A-n     Forward line         A-p     Backward line          (Use C-A-* to create a cursor)\n\
+A-q     Match token forward  A-j     Match token backward   (Use C-* to create a cursor)\n\
+A-r     Search forward       C-r     Search backwards\n\
 \n\
-Key binding prefixes:\n\
+\n\
+More advanced key bindings are found by using a prefix:\n\
 A-x     Editor state commands\n\
 A-c     Cursor commands\n\
 A-g     Project or directory commands\n\
-\n\
-Configuration should be done by editing `custom/config.cpp`.\n\
 ");
         server.editor.create_buffer(splash_page);
 
