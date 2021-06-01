63b8e8d046a4863bc8d23b0cb1e28c8e9c18690f
Chris Gregory <czipperz@gmail.com>
2021-06-01 00:39:55 -0500

    Make build-*.ps1 exit 1 when run-build.ps1 fails

--- build-release-debug.ps1
+++ build-release-debug.ps1
@@ -2,6 +2,7 @@ Push-Location $(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
 
 try {
     ./run-build.ps1 build/release-debug RelWithDebInfo
+    if (!$?) { exit 1 }
 } finally {
     Pop-Location
 }
--- build-tracy.ps1
+++ build-tracy.ps1
@@ -2,6 +2,7 @@ Push-Location $(Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
 
 try {
     ./run-build.ps1 build/tracy RelWithDebInfo -DTRACY_ENABLE=1
+    if (!$?) { exit 1 }
 } finally {
     Pop-Location
 }
