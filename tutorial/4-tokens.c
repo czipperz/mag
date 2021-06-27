a4ce47ff61b641200033b0fb8e747a1265f69084
Chris Gregory <czipperz@gmail.com>
2021-06-26 21:54:45 -0700

    Add more tutorials and make some minor edits

--- tutorial/4-tokens.c
+++ tutorial/4-tokens.c
@@ -10,8 +10,8 @@
 /// then press `A-q` (forward token) then `A-j` (backward token).
 /// This will go to other usages of the `hist` token.
 ///
-/// If you use `C-q` and `C-j` instead of moving by
-/// token you will create a cursor at a matching token.
+/// If you use `C-q` and `C-j` then instead of moving the
+/// cursor, you will create a cursor at the matching token.
 ///
 /// Now rename `hist` to `history`.
 /// Again, you can use `C-g` to exit multi cursor mode.
