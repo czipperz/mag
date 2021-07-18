c4880bb13a090f1df093b853b40fe07e95f57f14
Chris Gregory <czipperz@gmail.com>
2021-07-17 20:58:37 -0700

    find_indent_width: UP_THEN_BACK_PAIR: handle unmatched open pair in middle of line when there is an unmatched open pair at the end of the line as well

--- src/indent.cpp
+++ src/indent.cpp
@@ -10,37 +10,57 @@ using namespace basic;
 
 uint64_t find_indent_width(Buffer* buffer, Contents_Iterator it) {
     switch (buffer->mode.discover_indent_policy) {
-    case Discover_Indent_Policy::UP_THEN_BACK_PAIR:
+    case Discover_Indent_Policy::UP_THEN_BACK_PAIR: {
+        uint64_t start_position = it.position;
+
         if (!backward_up_token_pair(buffer, &it)) {
             return 0;
         }
 
+        uint64_t state;
+        Token token;
+
         // If the open pair token is at the end of the line then indent starting after it.
         {
             Contents_Iterator token_iterator = it;
-            uint64_t state;
-            Token token;
             if (!get_token_after_position(buffer, &token_iterator, &state, &token)) {
                 token.end = it.position;
             }
             token_iterator.retreat_to(token.end);
-            if (!at_end_of_line(token_iterator)) {
+            if (!at_end_of_line(token_iterator) && token.end != start_position) {
                 return get_visual_column(buffer->mode, token_iterator);
             }
         }
 
         // This is super inefficient but it's not invoked programmatically so it's not a big deal.
         while (1) {
+            // If at the start of the line then indent one more level.
             Contents_Iterator solt = it;
             start_of_line_text(&solt);
             if (solt.position == it.position) {
                 return get_visual_column(buffer->mode, it) + buffer->mode.indent_width;
             }
 
-            if (!backward_token_pair(buffer, &it)) {
+            // We found an open pair but no other tokens so just indent one level.
+            if (!get_token_before_position(buffer, &it, &state, &token)) {
                 return buffer->mode.indent_width;
             }
+
+            it.retreat_to(token.start);
+
+            // Indent starting after the open pair token.  We hit this case
+            // if the open pair we found above was at the end of the line.
+            if (token.type == Token_Type::OPEN_PAIR) {
+                it.advance_to(token.end);
+                return get_visual_column(buffer->mode, it);
+            }
+
+            // Find the corresponding token.
+            if (token.type == Token_Type::CLOSE_PAIR) {
+                backward_up_token_pair(buffer, &it);
+            }
         }
+    }
 
     case Discover_Indent_Policy::COPY_PREVIOUS_LINE: {
         start_of_line(&it);
