614e72f43e7dfd52ac20a0d11192322f6d509ce6
Chris Gregory <czipperz@gmail.com>
2021-08-12 15:31:30 -0700

    Fix compilation error

--- clients/ncurses.cpp
+++ clients/ncurses.cpp
@@ -336,7 +336,7 @@ void run(Server* server, Client* client) {
     while (1) {
         ZoneScopedN("ncurses main loop");
 
-        client->update_mini_buffer_completion_cache(&server->editor);
+        client->update_mini_buffer_completion_cache();
         load_mini_buffer_completion_cache(server, client);
 
         render(&total_rows, &total_cols, cellss, &window_cache, &mini_buffer_window_cache,
