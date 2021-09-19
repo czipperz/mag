35de6bba2c293173e19cb4d3725c8aafea13d692
Chris Gregory <czipperz@gmail.com>
2021-09-19 00:17:03 -0700

    Bind C-A-c to gnu_global::command_complete_at_point

--- custom/config.cpp
+++ custom/config.cpp
@@ -427,8 +427,10 @@ static void create_key_map(Key_Map& key_map) {
 
     BIND(key_map, "A-g A-t", gnu_global::command_lookup_at_point);
     BIND(key_map, "A-g t", gnu_global::command_lookup_prompt);
-    BIND(key_map, "C-c", command_complete_at_point_nearest_matching);
+    BIND(key_map, "C-A-c", gnu_global::command_complete_at_point);
     BIND(key_map, "MOUSE3", gnu_global::command_move_mouse_and_lookup_at_point);
+
+    BIND(key_map, "C-c", command_complete_at_point_nearest_matching);
 
     BIND(key_map, "C-l", command_goto_center_of_window);
     BIND(key_map, "A-l", command_center_in_window);
