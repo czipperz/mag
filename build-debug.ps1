843c93aab5b120cb32bf2f7d979099eeef90e179
Chris Gregory <czipperz@gmail.com>
2022-12-12 01:04:27 -0800

    Change test executable name to mag-test

--- build-debug.ps1
+++ build-debug.ps1
@@ -4,7 +4,7 @@ try {
     ./run-build.ps1 build/debug Debug
     if (!$?) { exit 1 }
 
-    ./build/debug/test --use-colour=no
+    ./build/debug/mag-test --use-colour=no
     if (!$?) { exit 1 }
 } finally {
     Pop-Location
