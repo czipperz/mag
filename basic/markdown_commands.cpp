7f10c8140a6e7668e31adf962bd6a227d113c848
Chris Gregory <czipperz@gmail.com>
2024-07-10 00:13:37 -0400

    markdown::reformat_at: handle ordered lists

--- basic/markdown_commands.cpp
+++ basic/markdown_commands.cpp
@@ -1,5 +1,6 @@
 #include "markdown_commands.hpp"
 
+#include <cz/format.hpp>
 #include "buffer.hpp"
 #include "command.hpp"
 #include "command_macros.hpp"
@@ -11,6 +12,8 @@
 namespace mag {
 namespace markdown {
 
+static bool is_at_number(Contents_Iterator it, size_t* continuation_columns);
+
 REGISTER_COMMAND(command_reformat_paragraph);
 void command_reformat_paragraph(Editor* editor, Command_Source source) {
     WITH_SELECTED_BUFFER(source.client);
@@ -19,29 +22,104 @@ void command_reformat_paragraph(Editor* editor, Command_Source source) {
     reformat_at(source.client, buffer, iterator);
 }
 
-void reformat_at(Client *client, Buffer *buffer, Contents_Iterator iterator) {
+void reformat_at(Client* client, Buffer* buffer, Contents_Iterator iterator) {
     start_of_line_text(&iterator);
+    uint64_t initial_position = iterator.position;
+    uint64_t first_valid_line = iterator.position;
 
-    // Don't reformat title lines.
-    if (looking_at(iterator, "#")) {
-        return;
-    }
-
+    uint64_t column = get_visual_column(buffer->mode, iterator);
     cz::Str rejected_patterns[] = {"#", "* ", "- ", "+ "};
-    if (basic::reformat_at(client, buffer, iterator, "* ", "  ", rejected_patterns)) {
-        return;
-    }
-    if (basic::reformat_at(client, buffer, iterator, "- ", "  ", rejected_patterns)) {
-        return;
-    }
-    if (basic::reformat_at(client, buffer, iterator, "+ ", "  ", rejected_patterns)) {
-        return;
-    }
 
-    // Backup: format as a paragraph.
-    if (basic::reformat_at(client, buffer, iterator, "", "")) {
-        return;
+    while (1) {
+        if (looking_at(iterator, "#") || at_end_of_line(iterator)) {
+            if (iterator.position == initial_position) {
+                // Don't reformat title lines or empty lines.
+                return;
+            } else {
+            vanilla:
+                // No pattern has matched so far, so just reformat as a vanilla paragraph.
+                iterator.advance_to(first_valid_line);
+                basic::reformat_at(client, buffer, iterator, "", "");
+                return;
+            }
+        }
+
+        uint64_t col = get_visual_column(buffer->mode, iterator);
+
+        // Format unordered lists.
+        for (size_t i = 1; i < CZ_DIM(rejected_patterns); ++i) {
+            cz::Str pat = rejected_patterns[i];
+            if (iterator.position == initial_position) {
+                if (looking_at(iterator, pat)) {
+                    basic::reformat_at(client, buffer, iterator, pat, "  ", rejected_patterns);
+                    return;
+                }
+            } else {
+                if (col + pat.len == column && looking_at(iterator, pat)) {
+                    basic::reformat_at(client, buffer, iterator, pat, "  ", rejected_patterns);
+                    return;
+                }
+            }
+        }
+
+        // Format ordered lists.
+        size_t continuation_columns;
+        if (is_at_number(iterator, &continuation_columns)) {
+            if (iterator.position == initial_position || col + continuation_columns == column) {
+                cz::String number = {};
+                CZ_DEFER(number.drop(cz::heap_allocator()));
+                buffer->contents.slice_into(cz::heap_allocator(), iterator,
+                                            iterator.position + continuation_columns, &number);
+
+                cz::String continuation = cz::format(cz::many(' ', continuation_columns));
+                CZ_DEFER(continuation.drop(cz::heap_allocator()));
+
+                basic::reformat_at(client, buffer, iterator, number, continuation,
+                                   rejected_patterns);
+                return;
+            }
+
+            // Note: the only fallthrough case here is where col == column.  This only
+            // happens if the cursor starts at the bottom line of the snippet below.
+            // In this case we treat the number as if it was part of the paragraph as
+            // a normal word.  But if the cursor is on the line starting with `2.`
+            // then we always format as if it was an independent one-line paragraph.
+            //
+            // ```
+            // My nephew is
+            // 2. He is well.
+            // Have a good one.
+            // ```
+        }
+
+        // If at a line that isn't part of this paragraph, or reach
+        // the start of the file, just fall back to vanilla reformat.
+        if (col != column || iterator.position == 0) {
+            goto vanilla;
+        }
+
+        first_valid_line = iterator.position;
+        start_of_line(&iterator);
+        backward_char(&iterator);
+        start_of_line_text(&iterator);
     }
+}
+
+static bool is_at_number(Contents_Iterator it, size_t* continuation_columns) {
+    uint64_t start = it.position;
+
+    // Eat numbers at the start.
+    if (!cz::is_digit(it.get()))
+        return false;
+    do {
+        it.advance();
+    } while (!it.at_eob() && cz::is_digit(it.get()));
+
+    if (!looking_at(it, ". "))
+        return false;
+
+    *continuation_columns = it.position + 2 /* strlen(". ") */ - start;
+    return true;
 }
 
 }
--- tests/test_reformat_commands.cpp
+++ tests/test_reformat_commands.cpp
@@ -1,7 +1,7 @@
 #include "test_runner.hpp"
 
-#include "basic/reformat_commands.hpp"
 #include "basic/markdown_commands.hpp"
+#include "basic/reformat_commands.hpp"
 
 using namespace mag;
 using namespace mag::basic;
@@ -181,5 +181,93 @@ TEST_CASE("reformat_at markdown staircase") {
     }
     SECTION("one line spacer") {
         run_each("\n");
+    }
+}
+
+// If at the first or third line, reformat into a single paragraph,
+// treating the '2.' as a normal word.  If on the line starting with
+// '2.', then treat the '2.' as starting a separate number paragraph.
+TEST_CASE("reformat_at markdown numbered list number considered word") {
+    char input[] =
+        "My nephew is\n"
+        "2. He is well.\n"
+        "Have a good one.";
+    cz::Str str_input = input;
+
+    for (uint64_t i = 0; i < CZ_DIM(input); ++i) {
+        Test_Runner tr;
+        tr.setup(input);
+
+        WITH_SELECTED_BUFFER(&tr.client);
+        buffer->mode.preferred_column = 80;
+
+        Contents_Iterator it = buffer->contents.iterator_at(i);
+        markdown::reformat_at(&tr.client, buffer, it);
+
+        if (i <= str_input.find_index('\n') || i > str_input.rfind_index('\n')) {
+            REQUIRE(buffer->contents.stringify(tr.allocator()) ==
+                    "My nephew is 2.  He is well.  Have a good one.");
+        } else {
+            REQUIRE(buffer->contents.stringify(tr.allocator()) == input);
+        }
+    }
+}
+
+// Tests that each of the 3 input paragraphs are reformatted
+// correctly when they are selected by the cursor.
+TEST_CASE("reformat_at markdown numbered list") {
+    char body1[] =
+        "1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+        "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+        "exercitation ullamco laboris nisi ut aliquip ex ea commodo\n";
+    char body2[] =
+        "2. this\n"
+        "   should be combined\n";
+    char body3[] =
+        "3. and so should\n"
+        "   this!";
+    char result1[] =
+        "1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\n"
+        "   tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam,\n"
+        "   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n";
+    char result2[] = "2. this should be combined\n";
+    char result3[] = "3. and so should this!";
+
+    cz::String body = cz::format(body1, body2, body3);
+    CZ_DEFER(body.drop(cz::heap_allocator()));
+
+    auto run_test_suite = [](cz::Str body, size_t start, size_t len, cz::Str result) {
+        for (size_t i = start; i < start + len; ++i) {
+            INFO("i: " << i);
+
+            Test_Runner tr;
+            tr.setup(body);
+
+            WITH_SELECTED_BUFFER(&tr.client);
+            buffer->mode.preferred_column = 80;
+
+            Contents_Iterator it = buffer->contents.iterator_at(i);
+            markdown::reformat_at(&tr.client, buffer, it);
+
+            REQUIRE(buffer->contents.stringify(tr.allocator()) == result);
+        }
+    };
+
+    SECTION("paragraph split") {
+        cz::String result = cz::format(result1, body2, body3);
+        CZ_DEFER(result.drop(cz::heap_allocator()));
+        run_test_suite(body, 0, strlen(body1), result);
+    }
+
+    SECTION("line combine 1") {
+        cz::String result = cz::format(body1, result2, body3);
+        CZ_DEFER(result.drop(cz::heap_allocator()));
+        run_test_suite(body, strlen(body1), strlen(body2), result);
+    }
+
+    SECTION("line combine 2") {
+        cz::String result = cz::format(body1, body2, result3);
+        CZ_DEFER(result.drop(cz::heap_allocator()));
+        run_test_suite(body, strlen(body1) + strlen(body2), strlen(body3), result);
     }
 }
