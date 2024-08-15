e888c3592d10df063ccee2a0ac7f881233421951
Chris Gregory <czipperz@gmail.com>
2024-08-15 04:34:35 -0400

    delete command_insert_source_template_from_header

--- basic/cpp_commands.hpp
+++ basic/cpp_commands.hpp
@@ -31,8 +31,6 @@ void command_make_indirect(Editor* editor, Command_Source source);
 
 void command_extract_variable(Editor* editor, Command_Source source);
 
-void command_insert_source_template_from_header(Editor* editor, Command_Source source);
-
 void command_insert_divider_60(Editor* editor, Command_Source source);
 void command_insert_divider_70(Editor* editor, Command_Source source);
 void command_insert_divider_80(Editor* editor, Command_Source source);
