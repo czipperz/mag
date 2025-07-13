452543d9da56cc6d9a2e88504700e8e0c1dc0479
Chris Gregory <czipperz@gmail.com>
2025-07-13 13:59:54 -0400

    Add command_toggle_highlight_on_buffer_token_at_point

--- src/basic/highlight_commands.cpp
+++ src/basic/highlight_commands.cpp
@@ -2,6 +2,7 @@
 
 #include "core/command_macros.hpp"
 #include "core/face.hpp"
+#include "core/movement.hpp"
 #include "core/overlay.hpp"
 #include "overlays/overlay_highlight_string.hpp"
 
@@ -48,6 +49,25 @@ void command_remove_highlight_from_buffer(Editor* editor, Command_Source source)
     dialog.prompt = "Remove highlight from buffer: ";
     dialog.response_callback = command_remove_highlight_from_buffer_callback;
     source.client->show_dialog(dialog);
+}
+
+REGISTER_COMMAND(command_toggle_highlight_on_buffer_token_at_point);
+void command_toggle_highlight_on_buffer_token_at_point(Editor* editor, Command_Source source) {
+    SSOStr query = {};
+    CZ_DEFER(query.drop(cz::heap_allocator()));
+
+    WITH_SELECTED_NORMAL_BUFFER(source.client);
+    get_token_at_position_contents(buffer, window->cursors[window->selected_cursor].point, &query);
+
+    for (size_t i = 0; i < buffer->mode.overlays.len; ++i) {
+        if (syntax::is_overlay_highlight_string(buffer->mode.overlays[i], query.as_str())) {
+            buffer->mode.overlays[i].vtable->cleanup(buffer->mode.overlays[i].data);
+            buffer->mode.overlays.remove(i);
+            return;
+        }
+    }
+    buffer->mode.overlays.reserve(1);
+    buffer->mode.overlays.push(syntax::overlay_highlight_string(highlight_face, query.as_str()));
 }
 
 }
--- src/basic/highlight_commands.hpp
+++ src/basic/highlight_commands.hpp
@@ -9,5 +9,7 @@ namespace basic {
 void command_add_highlight_to_buffer(Editor* editor, Command_Source source);
 void command_remove_highlight_from_buffer(Editor* editor, Command_Source source);
 
+void command_toggle_highlight_on_buffer_token_at_point(Editor* editor, Command_Source source);
+
 }
 }
